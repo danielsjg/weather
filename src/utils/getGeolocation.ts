53d70d50953d2a83bb14c326c9b0d4bb781a7019
Daniel Soares Jorge <d.soares@xsensors.ai>
2022-09-13 17:31:47 -0300

    Geolocation service types implemented

--- src/utils/getGeolocation.ts
+++ src/utils/getGeolocation.ts
@@ -1,21 +1,22 @@
 // Libraries
 import geolocation from '@react-native-community/geolocation';
 
-const getGeolocation = async () => {
-  return new Promise((resolve, reject) =>
-    geolocation.getCurrentPosition(
-      location =>
-        resolve({
-          lat: location.coords.latitude,
-          lon: location.coords.longitude,
-        }),
-      () => reject(undefined),
-      {
-        timeout: 15000,
-        maximumAge: 15000,
-        enableHighAccuracy: true,
-      },
-    ),
+const getGeolocation = () => {
+  return new Promise<{ lat: number; lon: number } | undefined>(
+    (resolve, reject) =>
+      geolocation.getCurrentPosition(
+        location =>
+          resolve({
+            lat: location.coords.latitude,
+            lon: location.coords.longitude,
+          }),
+        () => reject(undefined),
+        {
+          timeout: 15000,
+          maximumAge: 15000,
+          enableHighAccuracy: true,
+        },
+      ),
   );
 };
 
